49692d38139762bb73a5233b5ff499066f47a515
Ayaan Govil <38332695+Ayaan-Govil@users.noreply.github.com>
2022-11-18 09:22:40 -0800

    Fix for nested sequential command groups

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/shplib/commands/SequentialCommandGroup.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/shplib/commands/SequentialCommandGroup.java
@@ -4,16 +4,8 @@ import java.util.ArrayList;
 import java.util.Arrays;
 
 public class SequentialCommandGroup extends Command {
-    private final Command command;
-
     public SequentialCommandGroup(Command... commands) {
-        ArrayList<Command> nextCommands = new ArrayList<>(Arrays.asList(commands).subList(1, commands.length));
-        commands[0].then(nextCommands);
-        this.command = commands[0];
-    }
-
-    @Override
-    public void init() {
-        CommandScheduler.getInstance().scheduleCommand(command);
+        ArrayList<Command> nextCommands = new ArrayList<>(Arrays.asList(commands));
+        then(nextCommands);
     }
 }
