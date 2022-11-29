a84337d6ddb9f4db9a79f875096b8f8e26094091
Ayaan Govil <38332695+Ayaan-Govil@users.noreply.github.com>
2022-11-29 15:07:51 -0800

    Fixed SequentialCommandGroup fr this time

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/shplib/commands/SequentialCommandGroup.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/shplib/commands/SequentialCommandGroup.java
@@ -4,8 +4,17 @@ import java.util.ArrayList;
 import java.util.Arrays;
 
 public class SequentialCommandGroup extends Command {
+
     public SequentialCommandGroup(Command... commands) {
         ArrayList<Command> nextCommands = new ArrayList<>(Arrays.asList(commands));
-        then(nextCommands);
+        unpack(nextCommands);
+    }
+
+    private void unpack(ArrayList<Command> commands) {
+        for (Command command : commands) {
+            if (command instanceof SequentialCommandGroup) {
+                unpack(command.getNextCommands());
+            } else then(command);
+        }
     }
 }
